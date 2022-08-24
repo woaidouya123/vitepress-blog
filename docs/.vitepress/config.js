16068b880614583e747a50fded615e65a6f0c15b
woaidouya123 <cfeigood@qq.com>
2022-08-24 09:16:53 +0800

    fix: build base

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,4 +1,5 @@
 module.exports = {
     title: 'Do Not Go Gentle Into That Good Night',
-    description: 'Rage, rage against the dying of the light.'
+    description: 'Rage, rage against the dying of the light.',
+    base: '/blog/'
 }

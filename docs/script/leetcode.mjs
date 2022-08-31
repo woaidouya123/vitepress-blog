54bd9547c27e83e4f2256fd8333234df6aa2062d
woaidouya123 <cfeigood@qq.com>
2022-08-31 20:29:36 +0800

    fix: 题解文章展示修复

--- docs/script/leetcode.mjs
+++ docs/script/leetcode.mjs
@@ -2,8 +2,12 @@ import axios from 'axios';
 import * as fs from 'fs';
 import * as path from 'path';
 
-let url = 'https://leetcode.cn/graphql/noj-go/';
-let graphqlQuery = {
+const url = 'https://leetcode.cn/graphql/';
+const submitQueryUrl = 'https://leetcode.cn/graphql/noj-go/';
+const userName = 'woaidouya123';
+
+// 获取近一年提交
+const queryYearSubmit = (userSlug) => ({
     query: `query userProfileCalendar($userSlug: String!, $year: Int) {
         userCalendar(userSlug: $userSlug, year: $year) {
             streak
@@ -11,27 +15,342 @@ let graphqlQuery = {
             submissionCalendar
         }
     }`,
-    variables: { userSlug: 'woaidouya123' },
-}
+    variables: { userSlug },
+});
+
+
+// 获取最近题解
+const recentSolutionParams = (userSlug) => ({
+    query: `query profileSolutionArticles($userSlug: String!, $skip: Int, $first: Int) {
+        solutionArticles(userSlug: $userSlug, skip: $skip, first: $first) {
+            pageInfo {
+            hasNextPage
+            }
+            edges {
+            node {
+                title
+                slug
+                createdAt
+                question {
+                titleSlug
+                translatedTitle
+                questionFrontendId
+                }
+                upvoteCount
+                topic {
+                viewCount
+                }
+            }
+            }
+        }
+    }`,
+    variables: { userSlug },
+});
+
+// 获取题目
+const questionParams = (titleSlug) => ({
+    operationName: "questionData",
+    query: `query questionData($titleSlug: String!) {
+        question(titleSlug: $titleSlug) {
+          questionId
+          questionFrontendId
+          categoryTitle
+          boundTopicId
+          title
+          titleSlug
+          content
+          translatedTitle
+          translatedContent
+          isPaidOnly
+          difficulty
+          likes
+          dislikes
+          isLiked
+          similarQuestions
+          contributors {
+            username
+            profileUrl
+            avatarUrl
+            __typename
+          }
+          langToValidPlayground
+          topicTags {
+            name
+            slug
+            translatedName
+            __typename
+          }
+          companyTagStats
+          codeSnippets {
+            lang
+            langSlug
+            code
+            __typename
+          }
+          stats
+          hints
+          solution {
+            id
+            canSeeDetail
+            __typename
+          }
+          status
+          sampleTestCase
+          metaData
+          judgerAvailable
+          judgeType
+          mysqlSchemas
+          enableRunCode
+          envInfo
+          book {
+            id
+            bookName
+            pressName
+            source
+            shortDescription
+            fullDescription
+            bookImgUrl
+            pressImgUrl
+            productUrl
+            __typename
+          }
+          isSubscribed
+          isDailyQuestion
+          dailyRecordStatus
+          editorType
+          ugcQuestionId
+          style
+          exampleTestcases
+          jsonExampleTestcases
+          __typename
+        }
+      }
+      `,
+    variables: { titleSlug },
+});
+
+// 获取题解
+const articleParams = (slug) => ({
+    operationName: "solutionDetailArticle",
+    query: `query solutionDetailArticle($slug: String!, $orderBy: SolutionArticleOrderBy!) {
+        solutionArticle(slug: $slug, orderBy: $orderBy) {
+          ...solutionArticle
+          content
+          question {
+            questionTitleSlug
+            __typename
+          }
+          position
+          next {
+            slug
+            title
+            __typename
+          }
+          prev {
+            slug
+            title
+            __typename
+          }
+          __typename
+        }
+      }
+
+      fragment solutionArticle on SolutionArticleNode {
+        ipRegion
+        rewardEnabled
+        canEditReward
+        uuid
+        title
+        slug
+        sunk
+        chargeType
+        status
+        identifier
+        canEdit
+        canSee
+        reactionType
+        reactionsV2 {
+          count
+          reactionType
+          __typename
+        }
+        tags {
+          name
+          nameTranslated
+          slug
+          tagType
+          __typename
+        }
+        createdAt
+        thumbnail
+        author {
+          username
+          profile {
+            userAvatar
+            userSlug
+            realName
+            __typename
+          }
+          __typename
+        }
+        summary
+        topic {
+          id
+          commentCount
+          viewCount
+          __typename
+        }
+        byLeetcode
+        isMyFavorite
+        isMostPopular
+        isEditorsPick
+        hitCount
+        videosInfo {
+          videoId
+          coverUrl
+          duration
+          __typename
+        }
+        __typename
+      }
+      `,
+    variables: { orderBy: "DEFAULT", slug },
+});
+
 const options = {
     method: 'POST',
     url,
     headers: {},
-    data: graphqlQuery,
 };
 
-axios.request(options).then(res => {
-    const data = res.data.data;
+const submitOptions = {
+    method: 'POST',
+    url: submitQueryUrl,
+    headers: {},
+};
+
+// 获取近一年提交数据
+const generageYearSubmitFile = () => {
+    return axios.request({
+        ...submitOptions,
+        data: queryYearSubmit(userName)
+    }).then(res => {
+        const data = res.data.data;
+        const __dirname = path.resolve()
+        const filePath = path.resolve(
+            __dirname,
+            `docs/data/lcData.json`
+        );
+        fs.writeFileSync(
+            filePath,
+            JSON.stringify(data),
+            {
+                encoding: 'utf8',
+            }
+        );
+    })
+}
+
+// 获取最近的题解
+const getRecentSolution = () => {
+    return axios.request({
+        ...options,
+        data: recentSolutionParams(userName)
+    }).then(res => {
+        return res.data.data.solutionArticles.edges;
+    })
+}
+
+// 获取题解
+const getSolution = (slug) => {
+    return axios.request({
+        ...options,
+        data: articleParams(slug)
+    }).then(res => {
+        return res.data.data.solutionArticle;
+    })
+}
+
+// 获取题目
+const getQuestion = (title) => {
+    return axios.request({
+        ...options,
+        data: questionParams(title)
+    }).then(res => {
+        return res.data.data.question;
+    })
+}
+
+// 写入文件
+const generateMDFile = (data) => {
+    const content = parseTemplate(data);
     const __dirname = path.resolve()
     const filePath = path.resolve(
         __dirname,
-        `docs/data/lcData.json`
+        `docs/articles/leetcode/notes/${data.title}.md`
     );
     fs.writeFileSync(
         filePath,
-        JSON.stringify(data),
+        content,
         {
             encoding: 'utf8',
         }
     );
-})
+}
+
+const recentSolutins = await getRecentSolution();
+
+const processQuestionText = (text) => {
+    // 格式处理
+    text = text.replace(/<([ =]+)/g, '&lt;$1').replace(/\t/g, '  ').replace(/<(\/)?font.*?>/g, '');
+    return text;
+}
+
+const parseTemplate = ({title, link, question, solution}) =>
+`# [${title}](${link})
+
+## 题目
+${processQuestionText(question)}
+
+## 题解
+${solution}
+`
+
+for(let i=0; i<recentSolutins.length; i++){
+    const question = await getQuestion(recentSolutins[i].node.question.titleSlug);
+    const solution = await getSolution(recentSolutins[i].node.slug);
+    generateMDFile({
+        title: `${question.questionFrontendId}.${question.translatedTitle}`.replace(/\s/g, ''),
+        link: `https://leetcode.cn/problems/${question.titleSlug}`,
+        question: question.translatedContent,
+        solution: solution.content
+    })
+}
+
+const indexFileTemplate = (articles) => `# 算法笔记
+${articles.map(v => '[' + v.title + '](' + v.link + ')').join('\n\r')}
+`
+// 写入索引文件
+const generateIndexFile = () => {
+    const __dirname = path.resolve();
+    const dirPath = path.resolve(__dirname, `docs/articles/leetcode/notes/`);
+    const articles = fs.readdirSync(dirPath).map(v => ({
+        title: v.slice(0, -3),
+        link: `./notes/${v}`
+    }));;
+    const filePath = path.resolve(
+        __dirname,
+        `docs/articles/leetcode/index.md`
+    );
+    fs.writeFileSync(
+        filePath,
+        indexFileTemplate(articles),
+        {
+            encoding: 'utf8',
+        }
+    );
+}
+
+generateIndexFile();
+generageYearSubmitFile();
+

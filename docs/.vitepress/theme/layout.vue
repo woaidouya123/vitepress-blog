02a2090c5b65f6580bad72ebc669c87f3f12f0f8
woaidouya123 <cfeigood@qq.com>
2022-09-01 00:19:21 +0800

    feat: 修改滚动条样式

--- docs/.vitepress/theme/layout.vue
+++ docs/.vitepress/theme/layout.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { onMounted } from 'vue'
 import DefaultTheme from 'vitepress/theme'
+import { ElScrollbar } from 'element-plus'
+import 'element-plus/dist/index.css'
 const { Layout } = DefaultTheme
 onMounted(() => {
   import('../../utils/live2d').then(() => {
@@ -10,11 +12,14 @@ onMounted(() => {
 </script>
 
 <template>
-  <Layout>
-    <template #layout-bottom>
-      <canvas class="live2d" id="live2d" width="300" height="300"></canvas>
-    </template>
-  </Layout>
+  <el-scrollbar class="srcoll">
+    <Layout>
+      <template #layout-bottom>
+        <canvas class="live2d" id="live2d" width="300" height="300"></canvas>
+      </template>
+    </Layout>
+  </el-scrollbar>
+
 </template>
 <style scoped>
 .live2d {

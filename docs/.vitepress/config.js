a378d49e7c57f32f8e967d890be2911fe399f3c4
woaidouya123 <cfeigood@qq.com>
2022-09-06 09:43:32 +0800

    feat: 增加其他站点链接

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -9,6 +9,13 @@ module.exports = {
             { text: 'LeetCode', link: '/articles/leetcode/index' },
             { text: '前端笔记', link: '/articles/frontend/index' },
             { text: '杂七杂八', link: '/articles/others/index' },
+            {
+                text: '我的站点', items: [
+                    { text: 'FreshRSS', link: 'https://487lq34228.oicp.vip/' },
+                    { text: 'Jupyter', link: 'https://487lq34228.oicp.vip/jupyter' },
+                    { text: 'Jenkins', link: 'https://487lq34228.oicp.vip/jenkins/' },
+                ]
+            }
         ]
     }
 }

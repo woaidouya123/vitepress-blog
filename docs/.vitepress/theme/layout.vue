28bc323e99a6407d7b4608a48489e095f2d97d13
woaidouya123 <cfeigood@qq.com>
2022-09-15 10:11:14 +0800

    feat: 优化显示

--- docs/.vitepress/theme/layout.vue
+++ docs/.vitepress/theme/layout.vue
@@ -1,18 +1,18 @@
 <script setup>
-import { onMounted } from 'vue'
-import DefaultTheme from 'vitepress/theme'
-import { ElScrollbar, ElPopover } from 'element-plus'
-import ColorText from '../../components/colorText.vue'
-import 'element-plus/dist/index.css'
-const { Layout } = DefaultTheme
+import { onMounted } from "vue";
+import DefaultTheme from "vitepress/theme";
+import { ElScrollbar, ElPopover } from "element-plus";
+import ColorText from "../../components/colorText.vue";
+import "element-plus/dist/index.css";
+const { Layout } = DefaultTheme;
 onMounted(() => {
-  import('../../utils/live2d').then(() => {
-    loadlive2d("live2d", "/blog/tororo/tororo.model.json")
-  })
-})
+  import("../../utils/live2d").then(() => {
+    loadlive2d("live2d", "/blog/tororo/tororo.model.json");
+  });
+});
 const goGamePage = () => {
-  location.href = location.origin + '/blog/articles/games/gobang'
-}
+  location.href = location.origin + "/blog/articles/games/gobang.html";
+};
 </script>
 
 <template>
@@ -22,21 +22,31 @@ const goGamePage = () => {
         <div class="live2d">
           <el-popover placement="top" trigger="hover" :show-after="3000">
             <template #reference>
-              <canvas id="live2d" width="300" height="300"></canvas>
+              <canvas
+                id="live2d"
+                width="600"
+                height="600"
+                style="
+                   {
+                    width: 300px;
+                    height: 300px;
+                  }
+                "
+              ></canvas>
             </template>
             <template #default>
               <div class="popover-box">
-                <color-text text="来玩个游戏喵~" @click="goGamePage"></color-text>
+                <color-text
+                  text="来玩个游戏喵~"
+                  @click="goGamePage"
+                ></color-text>
               </div>
             </template>
           </el-popover>
-
         </div>
-
       </template>
     </Layout>
   </el-scrollbar>
-
 </template>
 <style scoped>
 .live2d {
